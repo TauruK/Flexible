b9c9d2f6a6dfa87909ba961501eb649f29863f4c
k <1025023440@qq.com>
2018-09-17 01:20:57 +0800

    versions 1.0 1.打好框架基本目录结构 2.实现index.php?p=admin&m=test&a=test的基本url访问 3.引入Smarty模版引擎作为框架的模版引擎

---
+++ application/admin/controller/TestController.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\admin\controller;
+class TestController{
+	
+	public function test(){
+		$smarty = new \Smarty();
+		//构造smarty模版目录
+		$tmplateDir = 'E:/Flexible/application/'.$GLOBALS['p'].'/view/';
+		$smarty->setTemplateDir($tmplateDir);
+		
+		//构造smarty编译缓存目录
+		$complieDir = 'E:/Flexible/application/'.$GLOBALS['p'].'/view_c/';
+		$smarty->setCompileDir($complieDir);
+		
+		$smarty->assign('smarty','哈哈');
+		$smarty->display('test/test.html');
+	}
+	
+}
+?>
